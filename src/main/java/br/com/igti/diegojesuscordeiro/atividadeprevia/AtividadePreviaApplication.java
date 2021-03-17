b06853e96ee1789aefcb9dd85f45d354f7c11e09
Diego de jesus Cordeiro <diego.alura@gmail.com>
2021-03-17 18:09:00 -0300

    first commit

---
+++ src/main/java/br/com/igti/diegojesuscordeiro/atividadeprevia/AtividadePreviaApplication.java
@@ -0,0 +1,13 @@
+package br.com.igti.diegojesuscordeiro.atividadeprevia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadePreviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadePreviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igti/diegojesuscordeiro/atividadeprevia/AtividadePreviaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igti.diegojesuscordeiro.atividadeprevia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadePreviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
